6cb284cbcf45765404ca9a45ae5ee6af1ce3e20f
rjowers <rjowers@g.clemson.edu>
2017-06-15 14:58:49 -0400

    added basic styling and appending test data to template with Javascript

--- scripts/main.js
+++ scripts/main.js
@@ -16,6 +16,54 @@ function getData(data) {
 
 getData()
 
+function holdData(data, callback) {
+    $.ajax({
+        url: `https://api.darksky.net/forecast/d8fc3b794588208348c3f592cb9ce424/37.8267,-122.4233`,
+        dataType: "jsonp",
+        data: {
+            q: data
+        },
+        success: callback
+    })
+};
+
+function name() {
+    console.log(hello )
+}
+holdData()
+var putWeather;
+function appendData(){
+    console.log(inside)
+    for(var count = 0; count < data.currently.length; count++ ){
+        putWeather =$(".hourly-weather").html();
+        putWeather +=`
+        <p class="weather">$${data.currently.length}</p>
+
+        `;
+        $(".hourly-weather").html(putWeather);
+
+    }
+
+
+};
+
+
+// var putImages;
+// for (var count = 0; count < items.results.length; count++) {
+//     putImages = $(".alltheitems").html();
+//     putImages +=
+//         `<div class="image">
+//     <div class="icons">
+//     <img class="heart" src="heart.png"/>
+//     <img class="hamburger" src="hamburger.png"/>
+//     </div>
+//     <a href = "${items.results[count].url}"><img src="${items.results[count].Images[0].url_170x135}" width = "225"/></a>
+//     <p class="title">${items.results[count].title}</p>
+//     <p class="shopname">${items.results[count].Shop.shop_name}</p>
+//     <p class="money">$${items.results[count].price}</p>
+//    </div>`;
+//     $(".alltheitems").html(putImages);
+// };
 
 // $.getJSON('http://api.darksky.net/forecast/d8fc3b794588208348c3f592cb9ce424/37.8267,-122.4233', function(forecast) {});
 
